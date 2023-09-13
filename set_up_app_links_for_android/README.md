0c37e464d6bd772c26de4eca4e67e020b4eca0b8
Niranjan-KS <niranjan.ks.pro@gmail.com>
2023-09-13 18:57:53 +0530

    commit

---
+++ set_up_app_links_for_android/README.md
@@ -0,0 +1,3 @@
+# set_up_app_links_for_android
+
+A new Flutter project.
---
+++ set_up_app_links_for_android/android/app/src/main/kotlin/com/example/set_up_app_links_for_android/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.set_up_app_links_for_android
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
